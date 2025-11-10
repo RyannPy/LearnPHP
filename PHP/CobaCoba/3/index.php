14d01f4814b36442d15ad30f32a8c43df04fd539
RyanPy <ryanbayu1233@gmail.com>
2025-11-10 16:10:57 +0700

    Add files via upload

---
+++ PHP/CobaCoba/3/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Judul Halaman Web Saya</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+</head>
+<header class="header">
+  <h3 class="judulpage">YourNote</h3>
+  <a href="pages/login.php">Login</a>
+</header>
+<body>
+  <div class="halaman">
+    <h1 class="judulHome">Your Diary!</h1>
+  </div>
+  <div class="tampilancatatan">
+    <div class="card">
+      <h3 class="judulcatatan">Catatanmu</h3>
+      <p class="tanggalcatatan">02/10/2025</p>
+    </div>
+  </div>
+  
+</body>
+</html>
---
+++ PHP/CobaCoba/3/pages/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Judul Halaman Web Saya</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+</head>
+<header class="header">
+  <h3 class="judulpage">YourNote</h3>
+  <a href="register.php">Register</a>
+</header>
+<body>
+  <div class="halaman">
+    <h1 class="judul">Login</h1>
+    <form method="POST">
+      <input class="input" type="text" name="username" placeholder="Username"></input>
+      <input class="input" type="password" name="username" placeholder="Password"></input>
+      <button class="button" type="submit">Login</button>
+   </form>
+  </div>
+  
+</body>
+</html>
